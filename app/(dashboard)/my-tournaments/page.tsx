9f220cfd1a2ba7f5d92ec02f641afb8aab029da2
OscarCampohermoso <oscarcampohermoso8@gmail.com>
2024-12-10 11:49:43 -0400

    a litle improvement in tabs

--- app/(dashboard)/my-tournaments/page.tsx
+++ app/(dashboard)/my-tournaments/page.tsx
@@ -408,24 +408,29 @@ const TournamentsPage: React.FC = () => {
             )}
           </div>
           {/* Tabs */}
-          <div className="mb-6 border-b border-gray-200">
-            <nav className="-mb-px flex space-x-4">
-              {["Soon", "In Progress", "Finished"].map((status) => (
-                <button
-                  key={status}
-                  className={`px-4 py-2 border-b-2 font-medium text-sm ${
-                    activeTab === status
-                      ? "border-indigo-600 text-indigo-600"
-                      : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
-                  }`}
-                  onClick={() => setActiveTab(status)}
-                >
-                  {status}
-                </button>
-              ))}
-            </nav>
-          </div>
-          {renderTable(filteredTournaments, openModal, handleOpenTournamentDetails)}
+          {session && (
+          <>
+            {/* Tabs */}
+            <div className="mb-6 border-b border-gray-200">
+              <nav className="-mb-px flex space-x-4">
+                {["Soon", "In Progress", "Finished"].map((status) => (
+                  <button
+                    key={status}
+                    className={`px-4 py-2 border-b-2 font-medium text-sm ${
+                      activeTab === status
+                        ? "border-indigo-600 text-indigo-600"
+                        : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300"
+                    }`}
+                    onClick={() => setActiveTab(status)}
+                  >
+                    {status}
+                  </button>
+                ))}
+              </nav>
+            </div>
+            {renderTable(filteredTournaments, openModal, handleOpenTournamentDetails)}
+          </>
+        )}
         </CardContent>
       </Card>
   
